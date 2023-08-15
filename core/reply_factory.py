a7636d7e7963eba013775b6d2c71c76be93d9028
Harishkumar Yetam <119168445+harrypappi@users.noreply.github.com>
2023-08-15 22:50:29 +0530

    Update reply_factory.py

--- core/reply_factory.py
+++ core/reply_factory.py
@@ -10,22 +10,38 @@ def generate_bot_responses(message, session):
         bot_responses.append(BOT_WELCOME_MESSAGE)
 
     success, error = record_current_answer(message, current_question_id, session)
-
-    if not success:
-        return [error]
+    if current_question_id is not None:
+        session[f"answer_{current_question_id}"] = answer
+        return True, ""
+    else:
+        return False, "No current question ID."
 
     next_question, next_question_id = get_next_question(current_question_id)
 
-    if next_question:
-        bot_responses.append(next_question)
+     '''
+    Fetches the next question from the PYTHON_QUESTION_LIST based on the current_question_id.
+    '''
+
+    question_index = current_question_id + 1
+    if question_index < len(PYTHON_QUESTION_LIST):
+        return PYTHON_QUESTION_LIST[question_index], question_index
     else:
+        return None, -1
         final_response = generate_final_response(session)
         bot_responses.append(final_response)
+        
+total_score = 0
+    for question_id, question in enumerate(PYTHON_QUESTION_LIST):
+        user_answer = session.get(f"answer_{question_id}")
+        if user_answer is not None:
+            # Assuming you have a function to evaluate the correctness of the answer
+            # and assign a score
+            question_score = evaluate_answer(user_answer, question)
+            total_score += question_score
+    
+    final_result = f"Your total score is: {total_score}"
+    return final_result
 
-    session["current_question_id"] = next_question_id
-    session.save()
-
-    return bot_responses
 
 
 def record_current_answer(answer, current_question_id, session):
